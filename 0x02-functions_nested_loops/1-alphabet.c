1dbbdb0f22af606c2b08e694af6a3b760d101a74
Karisma-471 <okoyecharisma@gmail.com>
2023-04-25 09:51:20 -0700

    Print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_alphabet - print the alphabet in lowercase
+  *
+  * Return: Always 0
+  */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 97; j <= 122; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
+
