fc27512b49000aa93bbe2a391460496e4f3d2af4
Karisma-471 <okoyecharisma@gmail.com>
2023-04-27 10:50:09 -0700

    3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_numbers - print numbers from 0 to 9
+  * followed by a new line
+  * Return: Always 0
+  */
+void print_numbers(void)
+{
+	char j;
+
+	for (j = '0'; j <= '9'; j++)
+		_putchar(j);
+	_putchar('\n');
+}
