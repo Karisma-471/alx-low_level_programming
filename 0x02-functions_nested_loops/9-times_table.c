557a213e38cd3ae33b689de279da2e782f8dd900
Karisma-471 <okoyecharisma@gmail.com>
2023-04-26 22:06:32 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,28 +10,29 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			c = b * a;
-			if (b == 0)
+			c = a * b;
+			if ((c / 10) == 0)
 			{
-				_putchar(c + '0');
-			}
+				_putchar(' ');
+			_putchar(c + '0');
 
-			if (c < 10 && b != 0)
+			if (b == 9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+			}
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
-			} else if (c >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
+				if (b == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
