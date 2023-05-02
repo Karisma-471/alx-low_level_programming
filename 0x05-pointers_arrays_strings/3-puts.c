699cfef8848456b2f3c66e277c988beeaaccd2c5
Karisma-471 <okoyecharisma@gmail.com>
2023-05-02 14:36:53 -0700

    print string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _puts - print a string followed by a new line to stdout
+  * @str: A pointer that points the string to be printed
+  * Return: Always 0
+  */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str++);
+	}
+	putchar('\n');
+}
