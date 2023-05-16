d8928d2a5d43286a13c65d9eb79ab4f0426caff8
Karisma-471 <okoyecharisma@gmail.com>
2023-05-16 04:36:13 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,17 +11,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	unsigned int o;
 
-	if (size == 0)
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
+	{
 		return (NULL);
+	}
 
-	str = calloc(size, sizeof(char));
-
-	if (str == NULL)
-		return (NULL);
-
-	for (unsigned int i = 0; i < size; i++)
-		str[i] = c;
+	for (o = 0; o < size; o++)
+		str[o] = c;
 
 	return (str);
 }
