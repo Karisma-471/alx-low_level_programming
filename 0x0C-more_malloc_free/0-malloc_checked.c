dbd629219ae19363eeda1face5d7dfc1332112d7
Karisma-471 <okoyecharisma@gmail.com>
2023-05-18 15:32:29 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,26 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
 #include "main.h"
-
+#include <stdlib.h>
 
 /**
-  * malloc_checked - A function that allocates memory using malloc
-  * @b: the number of bytes to allocate
+  * malloc_checked - A function that allocates memory
+  * @b: the size to allocate
   *
-  * Return: pointer to the memory allocated
-  * exit with status 98
+  * Return: Nothing
   */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
-
+	
 	ptr = malloc(b);
 
-	if (!ptr)
-	{
-		exit (98);
-	}
+	if (ptr == NULL)
+		exit(98);
 
 	return (ptr);
 }
