55b0e43147abc748dcf9d27b2e6abab2097951d7
Karisma-471 <okoyecharisma@gmail.com>
2023-05-22 15:39:17 -0700

    A program that prints a file name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - write a program that prints the name of the file
+  *
+  * Return: 0 0n success
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
