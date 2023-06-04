57720fd8ac9cc717b57ab96d25e124cd931b6db4
Karisma-471 <okoyecharisma@gmail.com>
2023-06-04 10:07:41 -0700

    updated more malloc 101-mul.c file

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+/**
+  * main - a pfunction that multiplies two positive numbers
+  * @argc: number of arguments
+  * @argv: array of argument
+  *
+  * Return: 0 if successful or 98 if there's an error
+  */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int m, n;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	for (m = 1; m < argc; m++)
+	{
+		for (n = 0; argv[m][n] != '\0'; n++)
+		{
+			if (!isdigit(argv[m][n]))
+			{
+				printf("Error\n");
+				return (98);
+			}
+		}
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+	printf("%d\n", result);
+
+	return (0);
+}
