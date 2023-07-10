66d01c0bb97319b296e8fdf4ca9ea26c5f406c0e
root <okoyecharisma@gmail.com>
2023-07-10 13:40:56 -0700

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,15 +19,21 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len = 0; text_content[len];len++)
+			;
 	}
 
-	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (file_d == -1)
+		return (-1);
+
 	w = write(file_d, text_content, len);
 
-	if (file_d == -1 || w == -1)
+	if (w == -1)
+	{
+		close(file_d);
 		return (-1);
+	}
 
 	close(file_d);
 
